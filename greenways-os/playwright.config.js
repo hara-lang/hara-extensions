61d828bbf7ec6910eca23ac45b6d54c26f7e3a5b
Chris Zheng <zcaudate@users.noreply.github.com>
2026-08-07 02:16:39 +1000

    Remove greenways-os (moving to application/greenways-os)

--- greenways-os/playwright.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@playwright/test";
-export default defineConfig({
-  testDir: "test",
-  testMatch: "*.spec.js",
-  timeout: 30000,
-});
--- greenways-os/src/devtools.js
+++
@@ -1,1 +0,0 @@
-chrome.devtools.panels.create("hara", "", "src/panel.html");
